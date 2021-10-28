ddb2fbcb219330610e3fd278a6f6ab29380e1717
Ana Salazar <anamariasalazar2507@gmail.com>
2021-10-28 09:51:05 -0500

    fin

--- ParcialT2/src/main/javaa/ParcialSegundoCorte/cache.java
+++ ParcialT2/src/main/javaa/ParcialSegundoCorte/cache.java
@@ -10,6 +10,14 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class cache {
-    @Autowired
-    @Qualifier("HttpConnectionImpl")
+    public class cache {
+        @org.springframework.beans.factory.annotation.Autowired
+        @org.springframework.beans.factory.annotation.Qualifier("HttpConnectionImpl")
+        edu.eci.covid.connection.impl.HttpConnectionImpl http;
+        private java.util.concurrent.ConcurrentHashMap<java.lang.String,java.lang.String> countries;
+        private java.util.concurrent.ConcurrentHashMap<java.lang.String,java.time.LocalDateTime> time;
+
+        public cache()
+
+        public java.lang.String getCovidByCountry(java.lang.String name) throws java.io.IOException, edu.eci.covid.connection.covidException {  }
+    }
