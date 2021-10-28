2ece23aba597e8e8935414c2475f7b627d70066c
Ana Salazar <anamariasalazar2507@gmail.com>
2021-10-28 08:31:57 -0500

    controller, api y demas

---
+++ ParcialT2/src/main/javaa/ParcialSegundoCorte/Controller/CoronovirusStatsController.java
@@ -0,0 +1,28 @@
+package javaa.ParcialSegundoCorte.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.eci.arsw.ParcialSegundoCorte.Service.IWeatherService;
+
+@RestController
+@RequestMapping("/coronavirus")
+public class WeatherController {
+
+    @Autowired
+    IWeatherService ws;
+
+    @RequestMapping(value = "/{city}", method = RequestMethod.GET)
+    public ResponseEntity<?> getWeatherByCity(@PathVariable(name = "city") String city) {
+        try {
+            return new ResponseEntity<>(ws.getWeatherByCity(city), HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.toString(), HttpStatus.FORBIDDEN);
+        }
+    }
+}
---
+++ ParcialT2/src/main/javaa/ParcialSegundoCorte/Service/ParcialT2Application.java
@@ -0,0 +1,16 @@
+package javaa.servide.ParcialT2Application;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+    public class ParcialT2Application implements CommandLineRunner{
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParcialT2Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
